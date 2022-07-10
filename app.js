66bf0670b9c8c7f2eb125f994f39c418b061b02f
Tom Taylor <tomgtaylor@yahoo.com>
2022-07-09 20:53:32 -0700

    Added HTML and JS

---
+++ app.js
@@ -0,0 +1,19 @@
+
+let q1 = prompt("What is your first name?");
+alert(`Your name is ${q1}. Nice to meet you.`);
+console.log(q1);
+
+let q2 = prompt("What State or country do you live in?");
+alert(`Ok ${q1}, I understand you live in ${q2}.`)
+console.log(q2);
+
+let q3 = prompt("What is your favorite movie?");
+alert(`Your favorite movie is ${q3}.`)
+console.log(q3);
+
+let q4 = prompt("In your opinion, what is the best color?");
+alert (`${q4} is a decent color.`)
+console.log(q4);
+
+alert (`It appears your name is ${q1}, and you live in ${q2}. It also appears your favorite film is ${q3}, and you think the best color is ${q4}.`)
+document.write(`Your name is ${q1}, and you live in ${q2}. Your favorite film is ${q3}, and you think the best color is ${q4}.`)
